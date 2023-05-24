1548c39f005f04430bf8b5f63e67cad90b64b0a9
P-Heckmann <paul.he@posteo.de>
2023-05-24 11:35:30 +0200

    c

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -1,4 +1,16 @@
 import streamlit as st
-
+import gpxpy
+import pandas as pd
+from geopandas import GeoDataFrame as gdf
+from shapely.geometry import Point
+from datetime import datetime
 
 st.write("# Welcome to Fitness-tracker")
+
+
+# Load activity
+act_path = r"C:\Users\paulh\Desktop\Fitness\data\Single_runs\activity_10981175453.csv"
+
+act_df = pd.read_csv(act_path)
+
+st.dataframe(act_df)  # Same as st.write(df)
