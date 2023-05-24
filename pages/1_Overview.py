3af24aa9c894dd054d11bed5cb7e160373c52770
P-Heckmann <paul.he@posteo.de>
2023-05-24 11:38:29 +0200

    c

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -8,6 +8,8 @@ st.write("# Welcome to Fitness-tracker")
 # Load activity
 act_path = r"C:\Users\paulh\Desktop\Fitness\data\Single_runs\activity_10981175453.csv"
 
-act_df = pd.read_csv(act_path)
+path = r".\data\Single_runs\activity_10981175453.csv"
+
+act_df = pd.read_csv(path)
 
 st.dataframe(act_df)  # Same as st.write(df)
