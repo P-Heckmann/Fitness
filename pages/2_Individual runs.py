7e53910a9d92e047db22df0226f34008d6b87ab1
P-Heckmann <paul.he@posteo.de>
2023-05-31 15:33:36 +0200

    c

--- pages/2_Individual runs.py
+++ pages/2_Individual runs.py
@@ -9,14 +9,14 @@ import datetime
 
 
 # df_gpx = pd.read_pickle(r"./data/gpx-data.pkl")
-# df_tcx = pd.read_pickle(r"./data/tcx-data.pkl")
-# df_general_tcx = pd.read_pickle(r"./data/general-tcx-data.pkl")
+df_tcx = pd.read_pickle(r"./data/tcx-data.pkl")
+df_general_tcx = pd.read_pickle(r"./data/general-tcx-data.pkl")
 
 
-df_tcx = pd.read_pickle(r"C:\Users\paulh\Desktop\Fitness\data\merged-tcx-data.pkl")
-df_general_tcx = pd.read_pickle(
-    r"C:\Users\paulh\Desktop\Fitness\data\merged-general-tcx-data.pkl"
-)
+# df_tcx = pd.read_pickle(r"C:\Users\paulh\Desktop\Fitness\data\merged-tcx-data.pkl")
+# df_general_tcx = pd.read_pickle(
+#    r"C:\Users\paulh\Desktop\Fitness\data\merged-general-tcx-data.pkl"
+# )
 
 st.write("# Individual runs")
 
